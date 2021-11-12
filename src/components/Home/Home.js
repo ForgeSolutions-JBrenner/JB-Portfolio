e38819c1e51c27745d75b22cad5494e7cb3034e9
Brenner <justin.brenner@forgetechnologysolutions.com>
2021-11-11 22:12:14 -0500

    test

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -19,7 +19,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> JUSTIN BRENNER!</strong>
+                <strong className="main-name"> JUSTIN BRENNER</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
